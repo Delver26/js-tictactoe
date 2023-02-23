8a518654c9d3ff84695b7dc3d730900aa821b4a5
delver <github@delver.zone>
2023-02-23 22:35:33 +0000

    add function to play a single round of tictactoe

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-getComputerChoice();
-console.log(getComputerChoice());
+console.log(playRound("Rock", getComputerChoice()));
 
 function getComputerChoice() {
     let choice = Math.random();
@@ -8,5 +7,30 @@ function getComputerChoice() {
     } else if (choice >= 0.33 && choice < 0.66) {
         choice = "Paper";
     } else choice = "Scissors";
+    console.log(choice);
     return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let result;
+
+    if (playerSelection === computerSelection) {
+        result = `You both picked ${playerSelection}, it's a draw!`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        result = "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        result = "You Win! Scissors beats Paper";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        result = "You Lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "You Win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        result = "You Lose! Rock beats Scissors";
+    } else result = `Don't have a combo for ${playerSelection} and ${computerSelection}`;
+
+    return result;
 }
